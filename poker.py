a9f6a7dca9f14e9fbbe52dab6f5e9aa3c7d4f5ee
Scott Shuffler <scottshuffler@gmail.com>
2016-11-18 13:23:43 -0500

    Removed clutter

--- poker.py
+++ poker.py
@@ -1,24 +1,28 @@
-from deuces import Deck, Card, Deck, Evaluator
+from deuces import Deck, Evaluator
 
 deck = Deck()
 board = deck.draw(5)
 player1_hand = deck.draw(2)
 player2_hand = deck.draw(2)
 
-eval = Evaluator()
+evaluator = Evaluator()
 
-p1_score = eval.evaluate(board, player1_hand)
-p2_score = eval.evaluate(board, player2_hand)
-
-
-
-p1_class = eval.get_rank_class(p1_score)
-p2_class = eval.get_rank_class(p2_score)
+p1_score = evaluator.evaluate(board, player1_hand)
+p2_score = evaluator.evaluate(board, player2_hand)
 
 
-print "Player 1 hand rank = %d (%s)\n" % (p1_score, eval.class_to_string(p1_class))
 
-print "Player 2 hand rank = %d (%s)\n" % (p2_score, eval.class_to_string(p2_class))
+# p1_class = eval.get_rank_class(p1_score)
+# p2_class = eval.get_rank_class(p2_score)
+
+
+# print "Player 1 hand rank = %d \n" % (p1_score, eval.class_to_string(p1_class))
+
+# print "Player 2 hand rank = %d \n" % (p2_score, eval.class_to_string(p2_class))
+
+print "Player 1 hand rank = %d \n" % (p1_score)
+
+print "Player 2 hand rank = %d \n" % (p2_score)
 
 if p1_score < p2_score:
 	print "Player 1 wins!"
